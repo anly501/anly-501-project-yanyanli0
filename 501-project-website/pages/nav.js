61ad343a210adefc37b333b5ad5f68924fc636da
yl1494@georgetown.edu <yl1494@georgetown.edu>
2022-12-05 18:33:42 -0500

    GITHUB-UPLOAD:DATE-2022-12-05-TIME-18-33-40

---
+++ 501-project-website/pages/nav.js
@@ -0,0 +1,7 @@
+document.write('<ul id ="header">\
+<li><a href="../index.html">Home</a></li>\
+</li>\
+<li><a href="https://github.com/anly501/anly-501-project-yanyanli0/tree/main/codes">Code for Reference</a></li>\
+</li>\
+</ul>\
+');
